ca64599cea95a5520a543176aff4750e50cc5738
HFocus <keenandbuckley@gmail.com>
2019-09-30 21:07:27 -0600

    Begin work on spark drive train
    - optional in code between talon and spark drive train
    
    TODO
    - Finish the initSpark method

--- src/main/java/frc4388/robot/subsystems/Drive.java
+++ src/main/java/frc4388/robot/subsystems/Drive.java
@@ -12,6 +12,8 @@ import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -28,37 +30,35 @@ public class Drive extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  public static WPI_TalonSRX m_leftFrontMotor = new WPI_TalonSRX(RobotMap.DRIVE_LEFT_FRONT_CAN_ID);
-  public static WPI_TalonSRX m_rightFrontMotor = new WPI_TalonSRX(RobotMap.DRIVE_RIGHT_FRONT_CAN_ID);
-  public static WPI_TalonSRX m_leftBackMotor = new WPI_TalonSRX(RobotMap.DRIVE_LEFT_BACK_CAN_ID);
-  public static WPI_TalonSRX m_rightBackMotor = new WPI_TalonSRX(RobotMap.DRIVE_RIGHT_BACK_CAN_ID);
+  /* false for talon drivetrain, true for spark drivetrain */
+  public static boolean isSpark = true;
 
-  public static DifferentialDrive m_driveTrain = new DifferentialDrive(m_leftFrontMotor, m_rightFrontMotor);
+  /* talons */
+  public static WPI_TalonSRX m_leftFrontMotorTalon = new WPI_TalonSRX(RobotMap.DRIVE_LEFT_FRONT_CAN_ID);
+  public static WPI_TalonSRX m_rightFrontMotorTalon = new WPI_TalonSRX(RobotMap.DRIVE_RIGHT_FRONT_CAN_ID);
+  public static WPI_TalonSRX m_leftBackMotorTalon = new WPI_TalonSRX(RobotMap.DRIVE_LEFT_BACK_CAN_ID);
+  public static WPI_TalonSRX m_rightBackMotorTalon = new WPI_TalonSRX(RobotMap.DRIVE_RIGHT_BACK_CAN_ID);
+
+  /* sparks */
+  public static CANSparkMax m_leftFrontMotorSpark = new CANSparkMax(RobotMap.DRIVE_LEFT_FRONT_CAN_ID, MotorType.kBrushless);
+  public static CANSparkMax m_rightFrontMotorSpark = new CANSparkMax(RobotMap.DRIVE_RIGHT_FRONT_CAN_ID, MotorType.kBrushless);
+  public static CANSparkMax m_leftBackMotorSpark = new CANSparkMax(RobotMap.DRIVE_LEFT_BACK_CAN_ID, MotorType.kBrushless);
+  public static CANSparkMax m_rightBackMotorSpark = new CANSparkMax(RobotMap.DRIVE_RIGHT_BACK_CAN_ID, MotorType.kBrushless);
+
+  /* drivetrain */
+  public static DifferentialDrive m_driveTrain;
   
+
+  /* inputs */
   private static double m_inputMove, m_inputSteer;
 
-  public Drive(){
-    /* factory default values */
-    m_leftFrontMotor.configFactoryDefault();
-    m_rightFrontMotor.configFactoryDefault();
-    m_leftBackMotor.configFactoryDefault();
-    m_rightBackMotor.configFactoryDefault();
-
-    /* set back motors as followers */
-    m_leftBackMotor.follow(m_leftFrontMotor);
-    m_rightBackMotor.follow(m_rightFrontMotor);
-
-    /* set neutral mode */
-    m_leftFrontMotor.setNeutralMode(NeutralMode.Brake);
-    m_rightFrontMotor.setNeutralMode(NeutralMode.Brake);
-    m_leftFrontMotor.setNeutralMode(NeutralMode.Brake);
-    m_rightFrontMotor.setNeutralMode(NeutralMode.Brake);
-
-    /* flip input so forward becomes back, etc */
-    m_leftFrontMotor.setInverted(false);
-    m_rightFrontMotor.setInverted(false);
-    m_leftBackMotor.setInverted(InvertType.FollowMaster);
-    m_rightBackMotor.setInverted(InvertType.FollowMaster);
+  public Drive() {
+    if (isSpark) {
+      initSpark();
+    } 
+    else {
+      initTalon();
+    }
   }
 
   @Override
@@ -67,6 +67,37 @@ public class Drive extends Subsystem {
     m_inputSteer = -(OI.getInstance().getDriverController().getRightXAxis());
 
     m_driveTrain.arcadeDrive(m_inputMove, m_inputSteer);
+  }
+
+  private void initTalon() {
+    /* create drivetrain */
+    m_driveTrain = new DifferentialDrive(m_leftFrontMotorTalon, m_rightFrontMotorTalon);
+
+    /* factory default values */
+    m_leftFrontMotorTalon.configFactoryDefault();
+    m_rightFrontMotorTalon.configFactoryDefault();
+    m_leftBackMotorTalon.configFactoryDefault();
+    m_rightBackMotorTalon.configFactoryDefault();
+
+    /* set back motors as followers */
+    m_leftBackMotorTalon.follow(m_leftFrontMotorTalon);
+    m_rightBackMotorTalon.follow(m_rightFrontMotorTalon);
+
+    /* set neutral mode */
+    m_leftFrontMotorTalon.setNeutralMode(NeutralMode.Brake);
+    m_rightFrontMotorTalon.setNeutralMode(NeutralMode.Brake);
+    m_leftFrontMotorTalon.setNeutralMode(NeutralMode.Brake);
+    m_rightFrontMotorTalon.setNeutralMode(NeutralMode.Brake);
+
+    /* flip input so forward becomes back, etc */
+    m_leftFrontMotorTalon.setInverted(false);
+    m_rightFrontMotorTalon.setInverted(false);
+    m_leftBackMotorTalon.setInverted(InvertType.FollowMaster);
+    m_rightBackMotorTalon.setInverted(InvertType.FollowMaster);
+  }
+
+  private void initSpark(){
+
   }
 
   @Override
