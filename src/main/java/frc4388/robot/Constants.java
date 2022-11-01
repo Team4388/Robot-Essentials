3eeb756f372841446f66a6326c2bcb4221356fcb
66945 <54561572+66945@users.noreply.github.com>
2022-11-01 17:48:59 -0600

    Update Constants.java

--- src/main/java/frc4388/robot/Constants.java
+++ src/main/java/frc4388/robot/Constants.java
@@ -8,6 +8,7 @@
 package frc4388.robot;
 
 import frc4388.utility.LEDPatterns;
+import frc4388.utility.Gains;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
