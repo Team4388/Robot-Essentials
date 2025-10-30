fe3e1f74a3b2d9081f163b9369e2e2406520f634
Zach Wilke <83995467+C4llSqin@users.noreply.github.com>
2025-10-30 17:28:10 -0600

    Update BuildConstants.java

--- src/main/java/frc4388/robot/constants/BuildConstants.java
+++ src/main/java/frc4388/robot/constants/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot-Essentials";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 123;
-  public static final String GIT_SHA = "56841636f984f1a691134b4f1ae8862353846b54";
-  public static final String GIT_DATE = "2025-10-30 16:40:04 MDT";
+  public static final int GIT_REVISION = 124;
+  public static final String GIT_SHA = "f2946c11a94e33e93af21bb083922019cf77bd65";
+  public static final String GIT_DATE = "2025-10-30 17:19:46 MDT";
   public static final String GIT_BRANCH = "calibration-system";
-  public static final String BUILD_DATE = "2025-10-30 17:18:25 MDT";
-  public static final long BUILD_UNIX_TIME = 1761866305954L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-10-30 17:21:40 MDT";
+  public static final long BUILD_UNIX_TIME = 1761866500635L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
