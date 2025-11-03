41f6be4992c532cf392fe81d52a0da51c887d1cd
Michael Mikovsky <mira.j.maroni@gmail.com>
2025-11-02 22:00:59 -0700

    Robot Position in Shuffle Board

--- src/main/java/frc4388/utility/configurable/TunableNumber.java
+++ src/main/java/frc4388/utility/configurable/TunableNumber.java
@@ -1,8 +1,6 @@
 package frc4388.utility.configurable;
-import frc4388.robot.constants.Constants;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
@@ -17,7 +15,7 @@ public class TunableNumber {
         this.tab = Shuffleboard.getTab(tabName);
         this.defaultValue = defaultValue;
         this.entry = tab.add(name, defaultValue)
-                        .withWidget(BuiltInWidgets.kTextView)
+                        .withWidget(BuiltInWidgets.kTextView) // Use a text box for editing
                         .getEntry();
     }   
 
